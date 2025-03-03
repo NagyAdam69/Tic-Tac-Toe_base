2af503bd05f5f73211f5856e66feb3d558763838
Nagy Ádám <nagy.adam@diak.szbi-pg.hu>
2025-03-03 10:16:31 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,15 +1,22 @@
 def get_menu_option():
   '''
   Should print a menu with the following options:
-  print("1. Human vs Human\n")
-  print("2. Random AI vs Random AI\n")
-  print("3. Human vs Random AI\n")
-  print("4. Human vs Unbeatable AI\n")
+  print("1. Human vs Human")
+  print("2. Random AI vs Random AI")
+  print("3. Human vs Random AI")
+  print("4. Human vs Unbeatable AI")
 
   The function should return a number between 1-4.
   If the user will enter invalid data (for example 5), than a message will appear
   asking to input a new value.
   '''
+  print("1. Human vs Human")
+  print("2. Random AI vs Random AI")
+  print("3. Human vs Random AI")
+  print("4. Human vs Unbeatable AI")
+
+  jatekmod = int(input("Choose a gamemode (1-4): "))
+  if 
   pass
 
 
