06052928998a8c8feb37203403e51497986a12f8
SimonyiDaniel <simonyi.daniel.gyorgy@diak.szbi-pg.hu>
2025-03-05 13:28:12 +0100

    making_the_menu03.05

--- menu.py
+++ menu.py
@@ -5,30 +5,25 @@ def get_menu_option():
   print("2. Random AI vs Random AI")
   print("3. Human vs Random AI")
   print("4. Human vs Unbeatable AI")
-
+  
   The function should return a number between 1-4.
   If the user will enter invalid data (for example 5), than a message will appear
   asking to input a new value.
   '''
-
-
-  print('\t1. Human vs Human \n \t2. Random AI vs Random AI \n \t3. Human vs Random AI\n \t4. Human vs Unbeatable AI')
-  jatekmod = input("Choose a gamemode (1-4): ")
-  if jatekmod > 0 and jatekmod < 5:
-    return jatekmod
-  else:
+  def jatekmod_valasztas():
     print("\nPlease choose from the listed options: ")
+    print('\t1. Human vs Human \n \t2. Random AI vs Random AI \n \t3. Human vs Random AI\n \t4. Human vs Unbeatable AI')
+    jatekmod = input("Choose a gamemode (1-4): ")
+    return jatekmod
+  if jatekmod_valasztas() not in range(1, 5):
+    print('\n\nChoose a correct gamemode')
+    jatekmod_valasztas()
+    
+
 """"
   # run this file to test you have implemented correctly the function
   option = get_menu_option()
   print("If the user selected 1, it should print 1")
   print(option) """
 
-print(get_menu_option())
-
-state = True
-jatekk = input('chose')
-while state:
-  if jatekk not in range(1, 5):
-    print("valaszz ujra")
-    
+get_menu_option()
