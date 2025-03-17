674a3453c3da40d9e921889e7678850843ab6427
SimonyiDaniel <simonyi.daniel.gyorgy@diak.szbi-pg.hu>
2025-03-17 09:19:35 +0100

    valasztas_testing_push

--- test.py
+++ test.py
@@ -4,41 +4,43 @@ board = [
     [".", ".", "."],
     [".", ".", "."], 
     ]
-valasz = input('Valasz egy mezot (a1, a2, stb)')
 
-match valasz:
-    case "a1":
-        board[0][0] = 'X'
+while True:
+    valasz = input('Valasz egy mezot (a1, a2, stb)')
 
-    case "a2":
-        board[0][1] = 'X'
+    match valasz:
+        case "a1":
+            board[0][0] = 'X'
 
-    case "a3":
-        board[0][2] = 'X'
+        case "a2":
+            board[0][1] = 'X'
 
-    case "b1":
-        board[1][0] = 'X'
+        case "a3":
+            board[0][2] = 'X'
 
-    case "b2":
-        board[1][1] = 'X'
+        case "b1":
+            board[1][0] = 'X'
 
-    case "b3":
-        board[1][2] = 'X'
+        case "b2":
+            board[1][1] = 'X'
 
-    case "c1":
-        board[2][0] = 'X'
+        case "b3":
+            board[1][2] = 'X'
 
-    case "c2":
-        board[2][1] = 'X'
+        case "c1":
+            board[2][0] = 'X'
 
-    case "c3":
-        board[2][2] = 'X'
+        case "c2":
+            board[2][1] = 'X'
 
-print("   1   2   3")
-print(f"A   {board[0][0]} | {board[0][1]} | {board[0][2]}")
-print("   ---+---+---") 
-print(f"B   {board[1][0]} | {board[1][1]} | {board[1][2]}")
-print("   ---+---+---")
-print(f"C   {board[2][0]} | {board[2][1]} | {board[2][2]}")
-print("   ---+---+---")
+        case "c3":
+            board[2][2] = 'X'
 
+    print("   1   2   3")
+    print(f"A   {board[0][0]} | {board[0][1]} | {board[0][2]}")
+    print("   ---+---+---") 
+    print(f"B   {board[1][0]} | {board[1][1]} | {board[1][2]}")
+    print("   ---+---+---")
+    print(f"C   {board[2][0]} | {board[2][1]} | {board[2][2]}")
+    print("   ---+---+---")
+    valasz = None
