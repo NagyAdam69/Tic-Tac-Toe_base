335220475b13785fd45219d0cd6cd1f9a0fd7962
Nagy Ádám <nagy.adam@diak.szbi-pg.hu>
2025-03-18 08:55:58 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,66 +4,88 @@ board = [
     [".", ".", "."]
     ]
 
+def display_board():
+        print("\n   1   2   3")
+        print(f"A   {board[0][0]} | {board[0][1]} | {board[0][2]}")
+        print("   ---+---+---") 
+        print(f"B   {board[1][0]} | {board[1][1]} | {board[1][2]}")
+        print("   ---+---+---")
+        print(f"C   {board[2][0]} | {board[2][1]} | {board[2][2]}")
+        print("   ---+---+---")
+        valasz = None
+
 while True:
-    valasz = input('Valasz egy mezot (a1, a2, stb)')
-
-    match valasz:
-        case "a1"  :
-            if board[0][0] == 'X':
-                raise ValueError('This has been chosen before, choose again')
-            else:
-                board[0][0] = 'X'
-
-        case "a2":
-            if board[0][1] == 'X':
-                raise ValueError('This has been chosen before, choose again')
-            else:
-                board[0][1] = 'X'
-
-        case "a3":
-            board[0][2] = 'X'
-
-        case "b1":
-            board[1][0] = 'X'
-
-        case "b2":
-            board[1][1] = 'X'
-
-        case "b3":
-            board[1][2] = 'X'
-
-        case "c1":
-            board[2][0] = 'X'
-
-        case "c2":
-            board[2][1] = 'X'
-
-        case "c3":
-            board[2][2] = 'X'
-            
-    print("   1   2   3")
-    print(f"A   {board[0][0]} | {board[0][1]} | {board[0][2]}")
-    print("   ---+---+---") 
-    print(f"B   {board[1][0]} | {board[1][1]} | {board[1][2]}")
-    print("   ---+---+---")
-    print(f"C   {board[2][0]} | {board[2][1]} | {board[2][2]}")
-    print("   ---+---+---")
-    valasz = None
-
-
-    for row in board:
-        if row[0] ==  row[1] ==  row[2] :
-            print(f"Winner is {row[1]}")
+    while True:
+        display_board()
+        for row in board:
+            if row[0] ==  row[1] ==  row[2] !="." :
+                print(f'Winner is player "{row[1]}"')
+                exit()
+                
+        for col in range(3):
+            if board[0][col] ==  board[1][col] == board[2][col] != "." :
+                print(f'Winner is player "{board[0][col]}"')
+                exit()
+        valasz = input('\nValasz egy mezot (a1, a2, stb)')
+        
+        if board[0][col] ==  board[1][col] == board[2][col] != "." :
+            print(f'Winner is player "{board[0][col]}"')
             exit()
             
-    for i in board:
-        column = 0
-        if board[0][column] == "X" and board[1][column] == "X" and board[2][column] == "X":
-            print("Winner is X")
-            exit()
-        elif board[0][column] == "O" and board[1][column] == "O" and board[2][column] == "O":
-            print("Winner is O")
-            exit()
-        column += 1
+        
+        match valasz:
+            case "a1":
+                    if board[0][0] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[0][0] = 'X'
+                        break
+            case "a2":
+                    if board[0][1] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[0][1] = 'X'
+                        break
+            case "a3":
+                    if board[0][2] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[0][2] = 'X'
+                        break
 
-
+            case "b1":
+                    if board[1][0] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[1][0] = 'X'
+                        break
+            case "b2":
+                    if board[1][1] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[1][1] = 'X'
+                        break
+            case "b3":
+                    if board[1][2] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[1][2] = 'X'
+                        break
+            case "c1":
+                    if board[2][0] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[2][0] = 'X'
+                        break
+            case "c2":
+                    if board[2][1] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[2][1] = 'X'
+                        break
+            case "c3":
+                    if board[2][2] != '.':
+                        print('This has been chosen before, choose again.')
+                    else:
+                        board[2][2] = 'X'
+                        break
