6d5afae4c7eb3012a7507e7e44f571aa476a8c7a
SimonyiDaniel <simonyi.daniel.gyorgy@diak.szbi-pg.hu>
2025-03-27 10:46:53 +0100

    Making login work

--- asd.py
+++ asd.py
@@ -1,24 +1,45 @@
-felhasz = input("Felhasználónév: ")
-jelszo = input("Jelszó: ")
+def bejelentkezes():
+    while True:
+        while True:
+            felhasz = ""
+            felhasz = input("Felhasználónév: ")
 
-fontos = []
-with open("felhasznalok.txt", "r", encoding="utf-8") as forrasfajl:
-    for sor in forrasfajl:
-        adatok = sor.strip().split(";")
-        felhasznalo = adatok[0]
-        jelszo_file = adatok[1]
-        pontszam = adatok[2]
-        fontos.append((felhasznalo, jelszo_file, pontszam))
+            fontos = []
+            with open("felhasznalok.txt", "r", encoding="utf-8") as forrasfajl:
+                for sor in forrasfajl:
+                    adatok = sor.strip().split(";")
+                    felhasznalo = adatok[0]
+                    jelszo_file = adatok[1]
+                    pontszam = adatok[2]
+                    fontos.append((felhasznalo, jelszo_file, pontszam))
 
-found = False
-for felhasznalo, jelszo_file, pontszam in fontos:
-    if felhasznalo == felhasz:
-        found = True
-        if jelszo == jelszo_file:
-            print(f"Sikeres bejelentkezés! Pontszám: {pontszam}")
+            found = False
+            for felhasznalo, jelszo_fasdile, pontszam in fontos:
+                if felhasznalo == felhasz:
+                    found = True
+                    jelszo = input("Jelszó: ")
+                    if jelszo == jelszo_file:
+                        print(f"Sikeres bejelentkezés! Pontszám: {pontszam}")
+                        exit()
+                    else:
+                        print("Hibás jelszó!")
+                        break
+                    break
+
+            if not found:
+                print("A felhasználó nem létezik!")
+                break
+while True:
+    while True:
+        sign_or_log = input("1. Bejelentkezés\n2. Regisztráció\n(1 vagy 2): ")
+
+        if sign_or_log == "1":
+            bejelentkezes() 
+            exit()
+
+        elif sign_or_log == "2":
+            regisztracio()
+            exit()
         else:
-            print("Hibás jelszó!")
-        break
-
-if not found:
-    print("A felhasználó nem létezik!")
+            print("Hibás bejelentkezés!")
+            break
